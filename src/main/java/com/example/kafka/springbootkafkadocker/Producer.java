dd0a129dac319a3e10d3d0b5ead2ee1b7035eca2
rajesh_kawali <Rajesh_Kawali@Dellteam.com>
2021-06-04 18:48:49 +0530

    intial commit

---
+++ src/main/java/com/example/kafka/springbootkafkadocker/Producer.java
@@ -0,0 +1,25 @@
+package com.example.kafka.springbootkafkadocker;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Producer {
+    private static final String TOPIC = "test_topic";
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        System.out.println("sendMessage");
+        this.kafkaTemplate.send(TOPIC,message);
+    }
+
+    @Bean
+    public NewTopic createTopic(){
+        System.out.println("createTopic");
+        return new NewTopic(TOPIC,1,(short) 1);
+    }
+}
